874bc200e8aeeebeda44a5f96398efb4343583e6
fergal achraf <farkalachraf@gmail.com>
2026-06-28 19:34:04 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,24 @@
 namespace App\Providers;
 
 use App\Models\souvenir;
-use App\Models\User;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
 
         Inertia::share('Filieres', function () {
@@ -37,10 +35,9 @@ class AppServiceProvider extends ServiceProvider
 
                 return $Filieres;
             }
+
             return [];
         });
-
-
 
         Inertia::share('Notifications', function () {
             if (auth()->user()) {
@@ -55,6 +52,7 @@ class AppServiceProvider extends ServiceProvider
 
                 return $notifications;
             }
+
             return [];
         });
     }
